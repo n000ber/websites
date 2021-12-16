c4ce289399261847d695c1cc556d6beab3e037e3
viplust2013@gmail.com <viplust2013@gmail.com>
2021-12-16 10:55:27 +0800

    landing page for recipes done

--- js/main.js
+++ js/main.js
@@ -2,14 +2,4 @@ $(document).ready(function()
 {    
     AOS.init();
     window.addEventListener('load', AOS.refresh);
-
-    //Click event handler for nav-items
-    $('.nav-item').on('click',function(){
-
-      //Remove any previous active classes
-      $('.nav-item').removeClass('active');
-
-      //Add active class to the clicked item
-      $(this).addClass('active');
-    });
 });
